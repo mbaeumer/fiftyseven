a27844c2c9c8ab87efe3b09b6ac90a48ce296189
Martin Bäumer <martin.baeumer@gmail.com>
2016-12-07 22:34:48 +0100

    started part1

---
+++ part1/ex1-hello.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+
+print("Exercise 1: saying hello")
+print("")
+name = input("What is your name? ")
+print("Hello, %s, nice to meet you!" % (name))
---
+++ part1/ex2-character-count.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+
+print("Exercise 2:")
+print("")
+input = input("Write something! ")
+print("%s has %d characters" % (input, len(input)))
